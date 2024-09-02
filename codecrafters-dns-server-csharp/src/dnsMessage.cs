69f5cb2f14ae05fa50f2fa808d632785a9bf3dfb
Hasibur Rahman Ridoy <hasibur.ridoy@northsouth.edu>
2024-09-02 17:02:33 +0600

    Changed the code a lot

--- codecrafters-dns-server-csharp/src/dnsMessage.cs
+++ codecrafters-dns-server-csharp/src/dnsMessage.cs
@@ -3,7 +3,84 @@ using System.Linq;
 using codecrafters_dns_server.src;
 
 namespace codecrafters_dns_server.src;
-class DnsMessage
+public class DnsMessage
+(byte[] udpPacket)
+{
+	private ushort Id
+	{
+		get; set;
+	} = (ushort)((udpPacket[0] << 8) | udpPacket[1]);
+	// ((udpPacket[2] >> 7) & 0x1) == 0;
+	private bool IsQuery { get; set; } = (udpPacket[2] & 0x80) == 0;
+	// udpPacket[2] >> 3
+	private int OpCode { get; set; } = udpPacket[2] & 0x78 & 0xF;
+	// udpPacket[2] >> 5) & 0x1
+	private bool AuthoritativeAnswer { get; set; } = (udpPacket[2] & 0x4) == 1;
+	// (udpPacket[2] >> 6) & 0x1
+	private bool Truncation { get; set; } = (udpPacket[2] & 0x2) == 1;
+	// (udpPacket[2] >> 7) & 0x1
+	private bool RecursionDesired { get; set; } = (udpPacket[2] & 0x80) == 1;
+	// (udpPacket[3] >> 7) & 0x1)
+	private bool RecursionAvailable { get; set; } = (udpPacket[3] & 0x80) == 1;
+	// (udpPacket[3] >> 4) & 0x0F
+	private int ResponseCode { get; set; } = udpPacket[3] & 0xF;
+	private int QuestionCount
+	{
+		get; set;
+	} = (ushort)((udpPacket[4] << 8) | udpPacket[5]);
+	private int AnswerRecordCount
+	{
+		get; set;
+	} = (ushort)((udpPacket[6] << 8) | udpPacket[7]);
+	private int AuthorityRecordCount
+	{
+		get; set;
+	} = (ushort)((udpPacket[8] << 8) | udpPacket[9]);
+	private int AdditionalRecordCount
+	{
+		get; set;
+	} = (ushort)((udpPacket[10] << 8) | udpPacket[11]);
+	public byte[] GetResponse()
+	{
+		// Convert the message to a byte array
+		var response = new byte[12];
+		response[0] = (byte)(Id >> 8);
+		response[1] = (byte)(Id & 0xFF);
+		IsQuery = false;
+		response[2] =
+			(byte)((IsQuery ? 0 : 1) << 7 | (OpCode & 0xF) << 3 |
+				   (AuthoritativeAnswer ? 1 : 0) << 2 | (Truncation ? 1 : 0) << 1 |
+				   (RecursionDesired ? 1 : 0));
+		response[3] =
+			(byte)((RecursionAvailable ? 1 : 0) << 7 | (ResponseCode & 0xF));
+		response[4] = (byte)(QuestionCount >> 8);
+		response[5] = (byte)(QuestionCount & 0xFF);
+		response[6] = (byte)(AnswerRecordCount >> 8);
+		response[7] = (byte)(AnswerRecordCount & 0xFF);
+		response[8] = (byte)(AuthorityRecordCount >> 8);
+		response[9] = (byte)(AuthorityRecordCount & 0xFF);
+		response[10] = (byte)(AdditionalRecordCount >> 8);
+		response[11] = (byte)(AdditionalRecordCount & 0xFF);
+		return response;
+	}
+	public sealed override string ToString()
+	{
+		return $"DnsMessage:\n" + $"  Id: {Id}\n" + $"  IsQuery: {IsQuery}\n" +
+			   $"  OpCode: {OpCode}\n" +
+			   $"  AuthoritativeAnswer: {AuthoritativeAnswer}\n" +
+			   $"  Truncation: {Truncation}\n" +
+			   $"  RecursionDesired: {RecursionDesired}\n" +
+			   $"  RecursionAvailable: {RecursionAvailable}\n" +
+			   $"  ResponseCode: {ResponseCode}\n" +
+			   $"  QuestionCount: {QuestionCount}\n" +
+			   $"  AnswerRecordCount: {AnswerRecordCount}\n" +
+			   $"  AuthorityRecordCount: {AuthorityRecordCount}\n" +
+			   $"  AdditionalRecordCount: {AdditionalRecordCount}";
+	}
+}
+
+
+/*class DnsMessage
 {
 	// Header
 	public ushort Id { get; set; }
@@ -58,4 +135,4 @@ class DnsMessage
 		// Return the byte array
 		return data;
 	}
-}
+}*/
